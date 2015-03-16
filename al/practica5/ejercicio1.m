269b61bfc716d90c02a29fc36c3f0c0124b12d0f
Grego Corpas <gregocorpas89@yahoo.es>
2015-03-16 12:27:18 +0100

    ejercicio1

---
+++ al/practica5/ejercicio1.m
@@ -0,0 +1,22 @@
+clc
+A=[1 0 -1; 1 1 2; 0 1 1]
+B=[2 1 1; 0 0 3; 1 -1 1]
+
+disp('A*B')
+A*B
+
+disp('B*A')
+B*A
+
+disp('(A+B)²')
+
+(A+B)^2
+
+disp('A² + 2AB + B²')
+A^2 + 2*A*B + B^2
+
+disp('(A+B)(A-B)')
+(A+B) * (A-B)
+
+disp('A² - B²')
+A^2 - B^2
